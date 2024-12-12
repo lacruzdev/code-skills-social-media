47b4efcff96ae67fa58465d8b46151d7999c7037
Enrique La Cruz <enrique@lacruz.dev>
2024-12-12 14:51:33 -0300

    Initial estructure: RESTFul API with .NET Core 3.1 - Part 2 - Clean Architecture

---
+++ SocialMedia.Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,25 @@
+﻿using SocialMedia.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Infrastructure.Repositories
+{
+    public class PostRepository
+    {
+        public IEnumerable<Post> GetPosts()
+        {
+            var posts = Enumerable.Range(1, 19).Select( p => new Post { 
+                PostId = p,
+                Description = $"Descripcion {p}",
+                Date = DateTime.Now,
+                ImageUrl = $"https://placehold.co/{p*50}x300",
+                UserId = p++,
+            });
+
+            return posts;
+        }
+    }
+}
